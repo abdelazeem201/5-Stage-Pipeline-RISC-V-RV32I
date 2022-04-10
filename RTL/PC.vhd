304eab421cc8bcd174391b150e9e8055f0e78991
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2022-04-10 11:49:49 +0200

    Update PC.vhd

--- RTL/PC.vhd
+++ RTL/PC.vhd
@@ -6,20 +6,20 @@ entity PC is
 	port(
 		PC_IN	: In	word;			--new program count
 		PC_OUT	: Out	word;			--current program count
-		EN		: In 	std_logic;		--enable active high
-		CLK		: In	std_logic;		--clock
-		RST		: In	std_logic		--asynchronous reset active low
+		EN		: In 	std_logic;	--enable active high
+		CLK		: In	std_logic;	--clock
+		RST		: In	std_logic	--asynchronous reset active low
 	);
 end entity PC;
 
 architecture behavioral of PC is
 
 begin
-	PC_proc: process(CLK, RST)			--asynchronous reset
+	PC_proc: process(CLK, RST)			
 	begin
-		if RST='0' then					--if reset is active
-			PC_OUT<=X"00400000";		--initialize de PC to the first instruction
-		elsif rising_edge(CLK) then 	--otherwise if there is a positive clock edge
+		if RST='0' then					
+			PC_OUT<=X"00400000";			--initialize de PC to the first instruction
+		elsif )CLK'event and CLK = '1') then 		--otherwise if there is a positive clock edge
 			if EN='1' then				--and enable is active
 				PC_OUT <= PC_IN; 		--writes the input on the output
 			end if;
